4bdf876020c2385510d8394905bffec08a050208
Anderson Almeida <anderson.almeida.21012@hotmail.com>
2023-07-12 12:47:48 -0300

    saving

--- app/src/main/java/com/calculadora/ninguchot/MainActivity.kt
+++ app/src/main/java/com/calculadora/ninguchot/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
     }
+        fun AcrescentearUmaExpressao(string: String, limpar_dados : Boolean){
+
+            if (resultado.text.isNotEmpty()){
+                expressao.text = " "
+            }
+
+            if (limpar_dados){
+                resultado.text = " "
+                expressao.append(String)
+            }else{
+                expressao.append(resultado.text)
+                expressao.append(string)
+                resultado.text = " "
+            }
+        }
 }
